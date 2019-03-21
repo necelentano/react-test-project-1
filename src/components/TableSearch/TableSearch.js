5ab7751ad4c1e3180c2a0ae86515a602062b2408
necelentano <necelentano@gmail.com>
2019-03-21 21:18:12 +0200

    Added PropTypes to TableSearch component

--- src/components/TableSearch/TableSearch.js
+++ src/components/TableSearch/TableSearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class TableSearch extends Component {
 
@@ -20,4 +21,8 @@ export default class TableSearch extends Component {
             </div>
         )
     }
+}
+
+TableSearch.propTypes = {
+    onSearch: PropTypes.func.isRequired
 }
