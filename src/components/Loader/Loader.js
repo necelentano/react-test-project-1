7347fa90b5db5e88269c04229c9732d73c9c98a5
necelentano <necelentano@gmail.com>
2019-03-29 23:27:02 +0200

    Refactored and added styles to Loader component

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -2,5 +2,11 @@ import React from 'react';
 
 import './Loader.css';
 
-export default () => <div className="lds-circle"><div></div></div>;
+export default () => {
+    return (
+        <div className="loader-wrapper">
+            <div className="lds-circle"><div></div></div>
+        </div>
+    )
+}
 
